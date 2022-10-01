b4f598f1c586d0079581d8ab2be89971ded60fd4
Tengyu Sun <tengyu.sun@kcl.ac.uk>
2022-10-01 18:15:47 +0100

    primes4

--- primes.py
+++ primes.py
@@ -12,9 +12,10 @@ def primes(number_of_primes):
         for i in range(2, (num//2)+1):
             if num%i == 0:
                 break
-            else:
-                list.append(num);
-                count +=1
+        else:
+            list.append(num);
+            count +=1
         num+=1
 
     return list
+
